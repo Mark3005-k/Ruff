e7db733dff3b3c9b2c6806f780f2154ca2087dcb
Mark3005-k <marchik.zd@gmail.com>
2024-09-29 03:01:10 -0600

    Add files via upload

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('logo').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.getElementById('menu').classList.toggle('active');
-    document.getElementById('cart_btn').classList.toggle('active');
-});
